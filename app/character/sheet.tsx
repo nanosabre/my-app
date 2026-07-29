4f820c60d682ec53abe0393c659cbaa31acb6092
Nanosabre <nanosabre@gmail.com>
2026-07-29 17:25:29 -0400

    Sheet outline

--- app/character/sheet.tsx
+++ app/character/sheet.tsx
@@ -1,7 +1,52 @@
+import "./sheet.css";
+
 export default function sheet() {
     return (
-    <div>
-        Character Sheet
+    <div className="sheet">
+        <div className="invPanel">
+            Inventory
+        </div>
+        <div className="mainPanel">
+            <div  className="mainName">
+                Name
+            </div>
+            <div className="mainArmor">
+                Armor
+            </div>
+            <div className="mainPicture">
+                Picture
+            </div>
+            <div className="mainHealthMana">
+                Health Mana
+            </div>
+            <div className="mainSkills">
+                Skills
+            </div>
+            <div className="mainConditions">
+                Conditions
+            </div>
+            <div className="mainFitness">
+                Fitness
+            </div>
+            <div className="mainFocus">
+                Focus
+            </div>
+            <div className="mainPrecision">
+                Precision
+            </div>
+            <div className="mainSense">
+                Sense
+            </div>
+            <div className="mainTalent1">
+                Talent 1
+            </div>
+            <div className="mainTalent2">
+                Talent 2
+            </div>
+        </div>
+        <div className="spellPanel">
+            Spells
+        </div>
     </div>
 )
 }
